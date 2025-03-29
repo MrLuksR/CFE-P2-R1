40842e55081bf1b452d354ff7565d15e4c6d13fc
Lucas Rangel <rangellucas2004@gmail.com>
2025-03-29 10:35:13 -0300

    Corrigiendo error

--- src/Ejercicio4.java
+++ src/Ejercicio4.java
@@ -4,12 +4,12 @@ public class Ejercicio4 {
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
 
-        System.out.println("Ingese la base del triangulo:");
-        double base = leer.nextDouble();
+        System.out.println("Ingese el largo del rectangulo:");
+        double largo = leer.nextDouble();
 
-        System.out.println("Ingese la altura del triangulo:");
-        double altura = leer.nextDouble();
+        System.out.println("Ingese el ancho del rectangulo:");
+        double ancho = leer.nextDouble();
 
-        System.out.println("El area de este triangulo es: " + (base*altura)/2);
+        System.out.println("El area de este rectangulo es: " + (largo*ancho));
     }
 }
